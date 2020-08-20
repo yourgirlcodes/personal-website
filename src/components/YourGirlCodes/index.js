4c40c1736b63996a7a9eb111982bb4c50ab47f11
yourgirlcodes <zoecohen01@gmail.com>
2020-08-20 12:45:59 +0300

    committingto revert from table

--- src/components/YourGirlCodes/index.js
+++ src/components/YourGirlCodes/index.js
@@ -3,7 +3,10 @@ import { MediumCarousel } from "./components/MediumCarousel";
 import { SideBar } from "./components/SideBar";
 import { BottomBar } from "./components/BottomBar";
 import { YGCIntro } from "./components/YGCIntro";
+<<<<<<< HEAD
 import {YGCHeader} from './components/YGCHeader'
+=======
+>>>>>>> parent of 13508db... changing yourgirlcodes to table
 import "./YourGirlCodes.css";
 
 export default class YourGirlCodes extends Component {
@@ -16,18 +19,24 @@ export default class YourGirlCodes extends Component {
     const breakpoint = 900;
     const screenType = width >= breakpoint ? "big" : "small";
     return (
+<<<<<<< HEAD
       <div className="YGC-container">
       <YGCHeader />
         {/* <SideBar /> */}
         {/* <BottomBar /> */}
         {/* <MediumCarousel /> */}
+=======
+>>>>>>> parent of 13508db... changing yourgirlcodes to table
         <YGCIntro />
 
         <SideBar />
         <BottomBar />
         <MediumCarousel />
+<<<<<<< HEAD
         </div>
 
+=======
+>>>>>>> parent of 13508db... changing yourgirlcodes to table
     );
   }
 }
